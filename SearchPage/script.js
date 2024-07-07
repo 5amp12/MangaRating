a6187b86651396626638d4d29742178c62e8092b
srgp2 <srgp2@kent.ac.uk>
2024-07-07 12:24:04 +0100

    added more bits to the search api section

--- SearchPage/script.js
+++ SearchPage/script.js
@@ -1,38 +1,57 @@
 $(document).ready(function(){
+
     $('#searchInput').on("keyup", function(event){
-        var searchValue = $('#searchInput').val();       //the search value
-        if(event.keyCode === 13){   //if Enter is pressed 
-            //API based code here:
-            const baseUrl = "https://api.mangadex.org/manga";
-            const params = new URLSearchParams({
-                title: searchValue
-            }).toString();
-            const url = `${baseUrl}?${params}`;
-            fetch(url, { 
-                method: 'GET'
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Network response was not ok: ${response.statusText}`); 
+        mainSection();
+        function mainSection(){
+            var searchValue = $('#searchInput').val();       //the search value
+            if(event.keyCode === 13){   //if Enter is pressed 
+                //API based code here:
+                const baseUrl = "https://api.mangadex.org/manga";
+                const params = new URLSearchParams({
+                    title: searchValue
+                }).toString();
+                const url = `${baseUrl}?${params}`;
+                fetch(url, { 
+                    method: 'GET'
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`Network response was not ok: ${response.statusText}`); 
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(url);
+                    console.log(data);
+
+                    const firstManga = data.data[0];
+                    console.log(firstManga);
+
+                    titleSection(data);        //getting the titles for each object
+                    // console.log(mangaTitle);
+
+                    const coverIdUrl = data.data[0].id;
+                    coverSection(data, coverIdUrl, 0, 0);
+                    // console.log("Cover ID: ", coverIdUrl);
+                }) 
+            }
+        }
+        function coverSection(data, coverIdUrl, counter, mangaNumber){
+            console.log(coverIdUrl);
+            for (let i=mangaNumber; i<3; i++){
+                let callingCount = 0; 
+                for (let x=counter; x<5; x++){
+                    console.log("the counter: ",counter);
+                    console.log(data.data[i].relationships[x].type)
+                    if ((data.data[i].relationships[x].type) = "cover_art"){
+                        console.log("found");
+                        callingCount = x;
+                        break;
+                    }
                 }
-                return response.json();
-            })
-            .then(data => {
-                console.log(url);
-                console.log(data);
-
-                const firstManga = data.data[0];
-                console.log(firstManga);
-    
-                const mangaTitle = data.data[0].attributes.title.en;
-                console.log(mangaTitle);
-
-                const coverIdUrl = data.data[0].id;
-                console.log("Cover ID: ", coverIdUrl);
-
-                const coverIdForFileName = data.data[0].relationships[2].id
-                console.log("Cover ID needed for getting cover data: ", coverIdForFileName);
-
+                coverIdForFileName = data.data[i].relationships[counter].id;
+                //console.log("Cover ID needed for getting cover data: ", coverIdForFileName);
+                console.log("THE MANGA SECTION", i);
                 //GETTING THE COVER ART
                 fetch(`https://api.mangadex.org/cover/${coverIdForFileName}`, {
                     method: 'GET'
@@ -46,15 +65,52 @@ $(document).ready(function(){
                 .then(data => {
                     console.log("Cover Data:", data);
                     const coverFileName = data.data.attributes.fileName;
-                    console.log("Cover fileName: ", coverFileName);
+                    //console.log("Cover fileName: ", coverFileName);
                     const baseUrl = "https://uploads.mangadex.org/covers"; // Base URL for MangaDex covers
-                    const fullCoverUrl = `${baseUrl}/${coverIdUrl}/${coverFileName}.256.jpg`;
-                    console.log("The cover URL: ", fullCoverUrl);
+                    const fullCoverUrl = `${baseUrl}/${coverIdUrl}/${coverFileName}.512.jpg`;
+                    //console.log("The cover URL: ", fullCoverUrl);
+                    if (i==0){
+                        console.log("working 1");
+                        $('#image-one').attr('src', fullCoverUrl);
+                    }
+                    else if (i==1){
+                        console.log("working 2");
+                        $('#image-two').attr('src', fullCoverUrl);
+                    }
+                    else if (i==2){
+                        $('#image-three').attr('src', fullCoverUrl);
+                    }
+                    
                 })
                 .catch(error => {
-                    console.error("Error fetching cover data:", error);
+                    if (counter != 5){
+                        console.log("calling count: ", callingCount);
+                        coverSection(data, coverIdUrl, (callingCount+1), i);
+                    }
+                    else{
+                        console.error("Error fetching cover data:", error);
+                    } 
                 })    
-            }) 
+            }
+        }
+
+        function titleSection(data){
+            for (let i=0; i<3; i++){
+                let mangaTitle = data.data[i].attributes.title.en;
+                
+                if (i==0){
+                    console.log("working 1");
+                    $('#title-one').text(mangaTitle);
+                }
+                else if (i==1){
+                    console.log("working 2");
+                    $('#title-two').text(mangaTitle);
+                }
+                else if (i==2){
+                    console.log("working 3");
+                    $('#title-three').text(mangaTitle);
+                }
+            }
         }
     })
 })
