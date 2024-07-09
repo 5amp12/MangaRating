0064b52b150dfa65583b8c5dc0fd7a7a2be18fda
srgp2 <srgp2@kent.ac.uk>
2024-07-09 16:06:42 +0100

    fixed up some errors - improved the search feature and made the MangaDetails file

--- SearchPage/script.js
+++ SearchPage/script.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
     $('#searchInput').on("keyup", function(event){
         mainSection();
         function mainSection(){
+            
             var searchValue = $('#searchInput').val();       //the search value
             if(event.keyCode === 13){   //if Enter is pressed 
+                $('.image-container').css('visibility' ,'visible');   //making divs visible
+                $('.information-container').css('visibility' ,'visible');
                 //API based code here:
                 const baseUrl = "https://api.mangadex.org/manga";
                 const params = new URLSearchParams({
@@ -27,81 +30,98 @@ $(document).ready(function(){
                     const firstManga = data.data[0];
                     console.log(firstManga);
 
-                    tagsSection(data);
-                    titleSection(data);        //getting the titles for each object
-                    descriptionSection(data)
-                    // console.log(mangaTitle);
 
-                    const coverIdUrl = data.data[0].id;
-                    coverSection(data, coverIdUrl, 0, 0);
-                    // console.log("Cover ID: ", coverIdUrl);
+                    //****THIS SECTION IS MAKING SURE THE TOP RESULT IS WHAT THE USER SEARCHED
+                    let checkingSearchVal = searchValue.replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+                    checkingSearchVal = checkingSearchVal.replaceAll(" ", "");
+                    checkingSearchVal = checkingSearchVal.toLowerCase();
+                    //code above getting rid of punctuation and spaces for search
+                    let found = false;
+                    for(let i=0; i<data.data.length; i++){
+                        let checkingResultValues = (data.data[i].attributes.title.en).replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+                        checkingResultValues = checkingResultValues.replaceAll(" ", "");    
+                        checkingResultValues = checkingResultValues.toLowerCase();
+                        //code above getting rid of punctuation and spaces for search RESULTS
+
+                        if (checkingSearchVal == checkingResultValues){
+                            console.log("hitting"); 
+                            tagsSection(data.data[i]);                     //the data.data[i] is getting that specific result, so the data can be used in the other functions
+                            titleSection(data.data[i], searchValue);        //getting the titles for each object
+                            descriptionSection(data.data[i])
+                        
+                            const coverIdUrl = data.data[i].id;
+                            coverSection(data.data[i], coverIdUrl, 0, 0);
+                            found = true;
+                            break;
+                        }
+                        //If the search is equal to one of the results, use that certain result
+                    }
+                    //if there is no match between search and result - use api's search guessing
+                    console.log(found);
+                    if (found != true){ 
+                        console.log("hitting"); 
+                        tagsSection(data.data[0]);
+                        titleSection(data.data[0], searchValue);        //getting the titles for each object
+                        descriptionSection(data.data[0])
+
+                        const coverIdUrl = data.data[0].id;
+                        coverSection(data.data[0], coverIdUrl, 0, 0);
+                    }
+                    //-----------------------
                 }) 
             }
         }
+
         function coverSection(data, coverIdUrl, counter, mangaNumber){
-            console.log(coverIdUrl);
-            for (let i=mangaNumber; i<3; i++){
-                let callingCount = 0; 
-                for (let x=counter; x<5; x++){
-                    console.log("the counter: ",counter);
-                    console.log(data.data[i].relationships[x].type)
-                    if ((data.data[i].relationships[x].type) = "cover_art"){
-                        console.log("found");
-                        callingCount = x;
-                        break;
-                    }
+            console.log(coverIdUrl); 
+            for (let x=counter; x<5; x++){
+                if ((data.relationships[x].type) = "cover_art"){
+                    callingCount = x;
+                    break;
                 }
-                coverIdForFileName = data.data[i].relationships[counter].id;
-                //console.log("Cover ID needed for getting cover data: ", coverIdForFileName);
-                console.log("THE MANGA SECTION", i);
-                //GETTING THE COVER ART
-                fetch(`https://api.mangadex.org/cover/${coverIdForFileName}`, {
-                    method: 'GET'
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error(`Failed to fetch cover ${coverId}: ${response.statusText}`);
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log("Cover Data:", data);
-                    const coverFileName = data.data.attributes.fileName;
-                    //console.log("Cover fileName: ", coverFileName);
-                    const baseUrl = "https://uploads.mangadex.org/covers"; // Base URL for MangaDex covers
-                    const fullCoverUrl = `${baseUrl}/${coverIdUrl}/${coverFileName}.512.jpg`;
-                    //console.log("The cover URL: ", fullCoverUrl);
-                    if (i==0){
-                        console.log("working 1");
-                        $('#image-one').attr('src', fullCoverUrl);
-                    }
-                    else if (i==1){
-                        console.log("working 2");
-                        $('#image-two').attr('src', fullCoverUrl);
-                    }
-                    else if (i==2){
-                        $('#image-three').attr('src', fullCoverUrl);
-                    }
-                    
-                })
-                .catch(error => {
-                    if (counter != 5){
-                        console.log("calling count: ", callingCount);
-                        coverSection(data, coverIdUrl, (callingCount+1), i);
-                    }
-                    else{
-                        console.error("Error fetching cover data:", error);
-                    } 
-                })    
             }
+            coverIdForFileName = data.relationships[counter].id;
+            //console.log("Cover ID needed for getting cover data: ", coverIdForFileName);
+            
+            //GETTING THE COVER ART
+            fetch(`https://api.mangadex.org/cover/${coverIdForFileName}`, {
+                method: 'GET'
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch cover ${coverId}: ${response.statusText}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Cover Data:", data);
+                const coverFileName = data.data.attributes.fileName;
+                //console.log("Cover fileName: ", coverFileName);
+                const baseUrl = "https://uploads.mangadex.org/covers"; // Base URL for MangaDex covers
+                const fullCoverUrl = `${baseUrl}/${coverIdUrl}/${coverFileName}.512.jpg`;
+                //console.log("The cover URL: ", fullCoverUrl);
+                $('#image-one').attr('src', fullCoverUrl);
+                
+                
+            })
+            .catch(error => {
+                if (counter != 5){
+                    console.log("calling count: ", callingCount);
+                    coverSection(data, coverIdUrl, (callingCount+1), 0);
+                }
+                else{
+                    console.error("Error fetching cover data:", error);
+                } 
+            })    
+            
         }
 
         function tagsSection(data){
             const container = $('.tags-container');
-                container.empty();
+            container.empty();
             const className = 'tags';
             for (let i=0; i<3; i++){
-                let tagsName = data.data[0].attributes.tags[i].attributes.name.en;
+                let tagsName = data.attributes.tags[i].attributes.name.en;
 
                 const $p = $('<p></p>');
                 $p.addClass(className);
@@ -112,15 +132,19 @@ $(document).ready(function(){
         }
 
         function titleSection(data){
-                const mangaTitle = data.data[0].attributes.title.en;
-                
-                console.log("working 1");
-                $('#title-one').text(mangaTitle);
-                
+            const container = $('.title-container');
+            container.empty();
+
+            const mangaTitle = data.attributes.title.en;
+
+            const $p = $('<p></p>');
+            $p.addClass("object-title");
+            $p.text(mangaTitle);
+            container.append($p);
         }
 
         function descriptionSection(data){
-            const description = data.data[0].attributes.description.en;
+            const description = data.attributes.description.en;
             console.log(description);
             let shortenedDescription = "";
             for (let i=0; i<description.length; i++){
@@ -134,7 +158,22 @@ $(document).ready(function(){
                     }
                 }
             }
-            $('#description-one').text(shortenedDescription);
+
+            const container = $('.description-container');
+            container.empty();
+
+            const $p = $('<p></p>');
+            $p.text(shortenedDescription);
+            container.append($p);
+
+
+            // $('#description-one').text(shortenedDescription);
         }
+        
+        $('#result-id').on("click", function(){
+            alert("hello");
+        })
     })
+    
+    
 })
